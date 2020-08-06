666c549e5691e80b9d41dbc09295008406ed21e5
brunoanken <brunoanken@gmail.com>
2020-08-06 15:25:35 -0300

    :wrench: Hard code test postgres database password in test.exs config file.

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :rockbanking, RockBanking.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "mysecretpassword",
   database: "rockbanking_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
