21e9a5fac6797a398af6057761a3fb5941d00ebe
brunoanken <brunoanken@gmail.com>
2020-08-06 17:17:36 -0300

    :bug: Create unique constraint in email column in users table.

---
+++ priv/repo/migrations/20200806201645_create_unique_constraint_in_email_column_in_users_table.exs
@@ -0,0 +1,7 @@
+defmodule RockBanking.Repo.Migrations.CreateUniqueConstraintInEmailColumnInUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create unique_index(:users, [:email])
+  end
+end
