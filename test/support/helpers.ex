381d4b1760817e096a90b382a4506b11a4c8bd3f
brunoanken <brunoanken@gmail.com>
2020-08-10 18:35:57 -0300

    :pencil: Add @moduledoc to RockBanking.TestHelpers.

--- test/support/helpers.ex
+++ test/support/helpers.ex
@@ -1,4 +1,7 @@
 defmodule RockBanking.TestHelpers do
+  @moduledoc """
+  Helper functions that canbe used across some different test suites.
+  """
   def get_changeset_from_operation({_, tuple}) do
     {:changeset, changeset, _} = tuple
     changeset
